1f454d62f5e742ab44048a969ef06bade5238c9c
JoaoPedroSchlindwein <joaopedroschlindwein@gmail.com>
2022-05-08 15:58:08 -0300

    Questao 01 a) b) c) d) e) + correção categoria minúscula

--- Questao01/main.c
+++ Questao01/main.c
@@ -1,33 +1,43 @@
-//c) Garantir que a quantidade seja maior que 0.
-
 #include <stdio.h>
-
+#include <ctype.h>
 int main(void) {
   char categoria;
-  int quantidade = 0, 
-    soma,
-    total;
-  
-  float media;
-  
+
+  int quantidade,
+  soma = 0,
+  total = 0;
+
+  float media = 0;
+
   printf("Informe a categoria: ");
-  scanf("%c", &categoria);
-  
-  while (categoria == 'A' || categoria == 'B' || categoria == 'C') {
-        
+  scanf("%s", &categoria);
+  categoria = toupper(categoria);
+
+  while (categoria == 'A' || categoria == 'B' || categoria == 'C')
+    {
+      quantidade = 0;
       do{
         printf("Informe a quantidade: ");
         scanf("%d", &quantidade);
+        
       }while (quantidade <= 0);
-      
+
       soma = soma + quantidade;
-      total ++;
-      media = soma / quantidade;
+      total++;
       printf("Informe a categoria: ");
-      setbuf(stdin,NULL);
-      scanf("%c", &categoria);
+      scanf("%s", &categoria);
+      categoria = toupper(categoria);
     }
-  printf("A media dos produtos eh %.2f", media);
+
+  if (total > 0){
+
+    media = (float)soma / (float)total;
+    printf("A media dos produtos eh %.2f", media);
+
+  }
+
+  else
+    printf("Nao ha produtos!");
   
   return 0;
 }
