8c18e0143700d494bf58917a97b9269d333241b4
JoaoPedroSchlindwein <joaopedroschlindwein@gmail.com>
2022-05-08 20:05:25 -0300

    Questão 07 a) b) e c)

--- Questao07/main.c
+++ Questao07/main.c
@@ -1,47 +1,53 @@
-/* 7 - A Ler a quantidade e o respectivo valor de
-produtos. Parar a leitura quando informado valor 0 ou
-negativo para a quantidade. Ler o valor somente se
-informada quantidade válida (positivo).
-a) Calcular o valor total da compra.
-*/
-  #include <stdio.h>
+#include <stdio.h>
 
 int main(void) {
-    int quantidade,
-        contadorProduto = 0;
-
-    float valorUnitario = 1, 
+    int quantidade, 
+      contadorProduto = 0,
+      somaCompraInt,
+      real,
+      centavo;
+    
+  
+  float valorUnitario = 1, 
         valorPorProduto = 0, 
         somaCompra = 0, 
         somaValorUnitario = 0, 
-        valorMedio;
+        valorMedio,
+        centavoFloat;
 
-    printf("Quantidade: ");
+    printf("Informe a quantidade de um produto: ");
     scanf("%d",&quantidade);
 
     while (quantidade > 0){
         do{
-            printf("Valor: ");
+            printf("Informe o valor unitario de um produto: ");
             scanf("%f",&valorUnitario);
+
             if (valorUnitario < 0)
                 printf("Eh menor que 0, valor nao contabilizado!\n");
         }while (valorUnitario < 0);
 
         valorPorProduto = valorUnitario * quantidade;
         somaCompra = somaCompra + valorPorProduto;
-        somaValorUnitario = somaValorUnitario + valorUnitario;
-        contadorProduto = contadorProduto + 1;
-        printf("\nQuantidade: ");
+        somaValorUnitario = somaValorUnitario + valorUnitario * quantidade;
+        contadorProduto = contadorProduto + quantidade;
+        printf("\nInforme a quantidade de um produto: ");
         scanf("%d",&quantidade);
     }
 
-    for (int i = 0; i < 25; i++){
-        printf("-");
-    }
+   
 
-    valorMedio = somaValorUnitario/contadorProduto;
-    printf("\nTotal final: %.2f\n",somaCompra);
-    printf("Valor medio dos itens: %.2f",valorMedio);
+    somaCompraInt = somaCompra * 100;
+    if (contadorProduto > 0)
+        valorMedio = somaValorUnitario/contadorProduto;
+    else
+        valorMedio = 0;
+    centavo = somaCompraInt % 100;
+    centavoFloat = (float)centavo / 100;
+    real = somaCompraInt / 100;
+    printf("\nVALOR TOTAL DA COMPRA: R$ %.2f\n",somaCompra);
+    printf("VALOR MEDIO DA COMPRA: R$ %.2f\n\n",valorMedio);
+    printf("O valor total da compra eh %d reais e %d centavos.\n",real, centavo);
 
     return 0;
 }
