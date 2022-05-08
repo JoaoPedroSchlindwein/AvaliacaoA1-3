50f40639935022fbed40b45e1a68a6552569a54f
JoaoPedroSchlindwein <joaopedroschlindwein@gmail.com>
2022-05-08 15:28:21 -0300

    Sem uso desnecessário de memória

--- Questao03/main.c
+++ Questao03/main.c
@@ -1,48 +1,70 @@
 #include <stdio.h>
 
 int main(void) {
-  int menor, maior, n, divisor, contadorDePrimos = 0, primo = 1;
+
+  int menor = 0,
+      maior = 0, 
+      numero,
+      numColunas, 
+      col = 1,
+      divisor, 
+      contadorDePrimos = 0, 
+      primo = 1, 
+      contadorDeColunas = 0;
   
-  menor = maior = 0;
   while (menor <= 1){
+
     setbuf(stdin,NULL);
     printf("Informe o valor do limite inferior do intervalo: ");
     scanf("%d",&menor);
     
   }
+
+  numero = menor;
+  
   while (maior < menor){
+  
     setbuf(stdin,NULL);
     printf("Informe o valor do limite superior do intervalo: ");
     scanf("%d", &maior);
     
   }
+  
   int listaDePrimos [maior];
   printf("Informe quantos numeros deseja imprimir por linha: ");
-  scanf("%d", &n);
-  
-  while (menor < maior){
+  scanf("%d", &numColunas);
+   
+  while (numero < maior){
     
-    divisor = 2;
-    
+    divisor = 2;  
     primo = 1;
-    while (divisor < menor){
-      if (menor % divisor == 0)
+   
+    while (divisor < numero){
+   
+      if (numero % divisor == 0)
         primo = 0;
-      divisor++;
+
+      divisor++; 
     }
-    if (primo){
-      listaDePrimos[contadorDePrimos] = menor;
-      
+
+    if (primo == 1){
+
+      printf("%d\t",numero);  
       contadorDePrimos += 1;
+      col += 1;
       
+      if  (col == numColunas){
+      
+        col = 0;
+        printf("\n");
+      
+      }
+    
     }
     
-    menor++;
+    numero++;
+  
   }
-  for (int i = 0; i < contadorDePrimos; i++){
-    printf("%d\t",listaDePrimos[i]);
-    if ((i+1)%n == 0)
-      printf("\n");
-  }
+  
   return 0;
 }
