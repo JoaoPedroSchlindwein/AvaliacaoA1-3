0aa517777e1a2d2b7dba910b6009fc8c6b06abd1
JoaoPedroSchlindwein <joaopedroschlindwein@gmail.com>
2022-05-08 15:35:10 -0300

    Questão 01 a)

---
+++ Questao01/main.c
@@ -0,0 +1,26 @@
+/*a) Ler a quantidade somente se a categoria é válida
+(categorias A, B e C válidas).*/
+#include <stdio.h>
+
+int main(void) {
+  char categoria;
+  int quantidade, soma, total;
+  float media;
+  printf("Informe a categoria: ");
+  scanf("%c", &categoria);
+  while (categoria == 'A' || categoria == 'B' || categoria == 'C')
+    {
+      
+      printf("Informe a quantidade: ");
+      scanf("%f", &quantidade);
+      
+      soma = soma + quantidade;
+      total ++;
+      media = soma / quantidade;
+      printf("Informe a categoria: ");
+      scanf("%c", &categoria);
+    }
+  printf("A media dos produtos eh %.2f", media);
+  
+  return 0;
+}
